da16149b369cda11e3b9cb15a8e4ea3a979e7685
amrsamii <amrsamy77@gmail.com>
2020-05-11 16:04:23 +0200

    Fix start fragment

--- app/src/main/java/com/bestcoach/workouttracker/ui/start/StartFragment.kt
+++ app/src/main/java/com/bestcoach/workouttracker/ui/start/StartFragment.kt
@@ -1,6 +1,5 @@
 package com.bestcoach.workouttracker.ui.start
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,9 +11,8 @@ import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.bestcoach.workouttracker.R
 import com.bestcoach.workouttracker.databinding.FragmentStartBinding
-import com.bestcoach.workouttracker.ui.MainActivity
 
-class StartFragment : Fragment()  {
+class StartFragment : Fragment() {
     private lateinit var binding: FragmentStartBinding
     private val viewModel by viewModels<StartViewModel> { defaultViewModelProviderFactory }
 
@@ -23,19 +21,27 @@ class StartFragment : Fragment()  {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentStartBinding.inflate(inflater,container,false).apply {
-            viewmodel=viewModel
+        binding = FragmentStartBinding.inflate(inflater, container, false).apply {
+            viewmodel = viewModel
         }
         binding.lifecycleOwner = this.viewLifecycleOwner
-        // access the items of the list
-        val Exercises = resources.getStringArray(R.array.Exercises)
-        val adapter = ArrayAdapter(requireContext(),
-            android.R.layout.simple_spinner_item, Exercises)
-        binding.spinner.adapter = adapter
 
-        viewModel.eventNavigateToCamera.observe(viewLifecycleOwner){
-            if(it){
-                findNavController().navigate(StartFragmentDirections.actionStartFragmentToCameraFragment(binding.spinner.selectedItem.toString()))
+        // access the items of the list
+        val exercises = resources.getStringArray(R.array.Exercises)
+
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.spinner_item, exercises
+        )
+        binding.workoutSpinner.adapter = adapter
+
+        viewModel.eventNavigateToCamera.observe(viewLifecycleOwner) {
+            if (it) {
+                findNavController().navigate(
+                    StartFragmentDirections.actionStartFragmentToCameraFragment(
+                        binding.workoutSpinner.selectedItem.toString()
+                    )
+                )
                 viewModel.doneNavigatingToCamera()
             }
         }
